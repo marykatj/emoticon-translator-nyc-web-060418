fb844b95fe9071b0d7d8525e14a2459f607416d2
Mary-Kate James <marykatj@gmail.com>
2018-05-24 16:32:33 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 require 'yaml'
 # require modules here
 
-def load_library(emoticons.yml)
-  translator = YAML.load_file
+def load_library
+  translator = YAML.load_file(emoticons.yml)
   translator
 end
 

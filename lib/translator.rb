ea051d16e8e7e545b17340d6e404ea5fe595730d
Mary-Kate James <marykatj@gmail.com>
2018-05-24 16:39:57 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,19 +5,19 @@ def load_library(path)
   translator = YAML.load_file(path)
   translator_hash = Hash.new
 
-  translator_hash["emoticon"] = Hash.new
-  translator_hash["meaning"] = Hash.new
+  translator_hash["get_emoticon"] = Hash.new
+  translator_hash["get_meaning"] = Hash.new
 
   translator.each do |english, emoticon_group|
-  translator_hash["emoticon"][emoticon_group.first] = emoticon_group.last
-  translator_hash["meaning"][emoticon_group.last] = english
+  translator_hash["get_emoticon"][emoticon_group.first] = emoticon_group.last
+  translator_hash["get_meaning"][emoticon_group.last] = english
  end
  emoticon_hash
 end
 
 def get_japanese_emoticon(path, english_emoticon)
   emoticon_hash = load_library(path)
-  translated_item = emoticon_hash["emoticon"][emoticon]
+  translated_item = emoticon_hash["get_emoticon"][emoticon]
   if translated_item == nil
     translated_item = "Sorry, that emoticon was not found"
   end
